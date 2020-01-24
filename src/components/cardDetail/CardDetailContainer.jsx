66b6d6ee6345b7e61d07ae99fbbb2cf24372c9dc
torkvell <tor.kvell@outlook.com>
2020-01-24 09:35:35 +0100

    added fetch to details page for each rover

--- src/components/cardDetail/CardDetailContainer.jsx
+++ src/components/cardDetail/CardDetailContainer.jsx
@@ -7,24 +7,25 @@ export default class CardDetailContainer extends Component {
   };
 
   componentDidMount() {
-    fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=BNhPJtnZK474mPjmaH2nBhjX5YjkQkMdCPHV3x7e`
-    )
-      .then(res => res.json())
-      .then(response => {
-        const { id } = this.props.match.params;
-        console.log("ID", id);
-        console.log("response.photos", response.photos);
-        console.log("response.photos[0].id", response.photos[0].id);
-        console.log(
-          "FILTER",
-          response.photos.filter(obj => (obj.id === id ? obj : 5))
-        );
-        // const dataArray = Object.values(response.photos[id]);
-        // console.log(dataArray);
-        // this.updateState(dataArray);
-      })
-      .catch(console.error);
+    const { id } = this.props.match.params;
+    const urls = [
+      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=BNhPJtnZK474mPjmaH2nBhjX5YjkQkMdCPHV3x7e",
+      "https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1000&api_key=BNhPJtnZK474mPjmaH2nBhjX5YjkQkMdCPHV3x7e",
+      "https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1000&api_key=BNhPJtnZK474mPjmaH2nBhjX5YjkQkMdCPHV3x7e"
+    ];
+    Promise.all(urls.map(url => fetch(url).then(resp => resp.json()))).then(
+      respArray => {
+        // put it in component local state
+        this.setState({
+          loading: false,
+          data: respArray[id]
+        });
+        console.log(this.state.data);
+      }
+    ).catch = error => {
+      // if the loading fails, set an error state
+      this.setState({ error: error });
+    };
   }
 
   updateState(dataArray) {
